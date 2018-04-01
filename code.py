3d7131036412e72bb4ae6207aa6cc9c1630db770
Ayodhyankit Paul <3x3paul@gmail.com>
2018-04-02 03:21:14 +0530

    Update code.py

--- code.py
+++ code.py
@@ -55,5 +55,33 @@ train = tf.train.AdamOptimizer().minimize(loss)
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    mode_run,emd_run,trans1,trans2,conca_run,conca2_run,result_ma_run,max_pro,accc=sess.run([model,lookup,transpo1,transpo2,concat,concat2,result_matmul,max_prob,acc],feed_dict={input_x:np.random.randint(0,10,[23,6]),output_y:np.random.randint(0,14,[23])})
+    mode_run,emd_run,trans1,trans2,conca_run,conca2_run,result_ma_run,max_pro,accc,sdf=sess.run([output,lookup,transpo1,transpo2,concat,concat2,result_matmul,max_prob,acc,_states_c],feed_dict={input_x:np.random.randint(0,10,[23,6]),output_y:np.random.randint(0,14,[23])})
+    print("embedding_output",emd_run.shape)  # 23 x 6 x 110
 
+    print("each_lstm_cell_output",sdf.c.shape)     # 23 x 109
+
+    print("combine_rnn_output",mode_run[0].shape)  #23 x 6 x 109
+
+    print("transpose1",trans1.shape)   #6 x 23 x 109
+    print("transpose2",trans2.shape)   #6 x 23 x 109
+
+    print("concat_output_layes",conca_run.shape) #23 x 218
+
+    print("states concat_output",conca2_run.shape) #23 x 218
+
+    print("result_of_matmul",result_ma_run.shape)  #23 x 14
+
+    print("max",max_pro)
+
+
+
+
+#embedding_output (23, 6, 110)
+# each_lstm_cell_output (23, 109)
+# combine_rnn_output (23, 6, 109)
+# transpose1 (6, 23, 109)
+# transpose2 (6, 23, 109)
+# concat_output_layes (23, 218)
+# states concat_output (23, 218)
+# result_of_matmul (23, 14)
+# max [10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10]
